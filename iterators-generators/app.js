061ba16ba70dfb0c7daa5e51c58e6a8cd9a1d10e
Muchori Joseph <muchorijoseph97@gmail.com>
2022-05-18 22:32:44 +0300

    Iterator & Generators

---
+++ iterators-generators/app.js
@@ -0,0 +1,55 @@
+/**
+ * Iterators ->
+ * Generators -> can return mutliple values
+ */
+function nameIterator(names) {
+  let nextIndex = 0;
+
+  return {
+    next: function () {
+      return nextIndex < names.length
+        ? { value: names[nextIndex++], done: false }
+        : { done: true };
+    },
+  };
+}
+// create an array of names
+const namesArr = ["Jack", "Elon", "Buffet"];
+// init the iterator
+const names = nameIterator(namesArr);
+console.log(names.next());
+console.log(names.next().value);
+console.log(names.next().value);
+console.log(names.next());
+
+/**
+ * Generator
+ */
+function* sayNames() {
+  yield "Jack";
+  yield "Joe";
+  yield "Devoe";
+}
+
+const name = sayNames();
+console.log(name.next());
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next().value);
+
+// id creator/ generator
+
+function* createIds() {
+  let index = 0;
+  while (true) {
+    yield index++;
+  }
+}
+
+const gen = createIds();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
